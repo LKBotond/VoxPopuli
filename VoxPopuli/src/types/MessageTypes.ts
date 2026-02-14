35c1d1cf23f5fc7647db064b76db5d1492de7ac2
LKBotond <l.kovacs.botond@gmail.com>
2026-02-14 12:39:50 +0200

    added message types to fascilitate script to script communication

---
+++ VoxPopuli/src/types/MessageTypes.ts
@@ -0,0 +1,33 @@
+import type { LoginRequest, RegistrationRequest } from "../contracts/Auth";
+import type { CommentRequest, CommentEditRequest } from "../contracts/Comment";
+
+// Base type
+export interface BaseMessage<Action extends string, Payload = undefined> {
+  action: Action;
+  payload: Payload;
+}
+
+// Auth
+export type LoginMessage = BaseMessage<"login", LoginRequest>;
+export type LogoutMessage = BaseMessage<"logout">;
+export type RegistrationMessage = BaseMessage<"register", RegistrationRequest>;
+
+//comment
+export type CommentGetMessage = BaseMessage<"getComments", string>;
+export type CommentMessage = BaseMessage<"comment", CommentRequest>;
+export type CommentEditMessage = BaseMessage<"edit", CommentEditRequest>;
+export type CommentDeleteMessag = BaseMessage<"deleteComment", string>;
+export type UnknownActionResponse = BaseMessage<
+  string,
+  { error: "Unknown action" }
+>;
+
+// Union type for all messages
+export type RuntimeMessage =
+  | LoginMessage
+  | RegistrationMessage
+  | CommentGetMessage
+  | CommentMessage
+  | CommentEditMessage
+  | CommentDeleteMessag
+  | UnknownActionResponse;
