b202d85663c515efda2aecedc95f85d9ecab99e7
LKBotond <l.kovacs.botond@gmail.com>
2026-02-14 12:57:17 +0200

    readded Background script with some tweaks, also added Tanstack for API calls

---
+++ VoxPopuli/src/background.ts
@@ -0,0 +1,61 @@
+import type { CommentEditRequest, CommentRequest } from "./contracts/Comment";
+import type { CommentDeleteMessag, RuntimeMessage } from "./types/MessageTypes";
+import type {
+  LoginRequest,
+  RegistrationRequest,
+  SessionToken,
+} from "./contracts/Auth";
+import { loadSession } from "./utils/helpers";
+import * as Api from "./api/VoxPopuliApi";
+
+//call the helpers
+chrome.runtime.onMessage.addListener(
+  (message: RuntimeMessage, _, sendResponse) => {
+    handleMessaging(message).then((response) => {
+      sendResponse(response);
+    });
+    return true;
+  },
+);
+
+async function handleMessaging(message: RuntimeMessage) {
+  try {
+    const sessionToken: SessionToken | undefined = await loadSession();
+
+    switch (message.action) {
+      case "login":
+        return await Api.login(message.payload as LoginRequest);
+
+      case "register":
+        return await Api.register(message.payload as RegistrationRequest);
+
+      case "getComments":
+        if (!sessionToken) throw new Error("Unauthorized");
+        return await Api.getComments(message.payload as string, sessionToken);
+
+      case "comment":
+        if (!sessionToken) throw new Error("Unauthorized");
+        return await Api.postComment(
+          message.payload as CommentRequest,
+          sessionToken,
+        );
+
+      case "edit":
+        if (!sessionToken) throw new Error("Unauthorized");
+        return await Api.editComment(
+          message.payload as CommentEditRequest,
+          sessionToken,
+        );
+      case "deleteComment":
+        if (!sessionToken) throw new Error("Unauthorized");
+        return await Api.deleteComment(message.payload as string, sessionToken);
+
+      default:
+        console.warn("Unknown action", message.action);
+        return { error: "Unknown action" };
+    }
+  } catch (error) {
+    console.error("Background script error:", error);
+    return { error: error instanceof Error ? error.message : "Unknown error" };
+  }
+}
